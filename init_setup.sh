04fe73666fca92d0c30a1fe99d85da4c34c6d618
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-20 06:42:33 +0200

    update init_setup with conda.yaml

--- init_setup.sh
+++ init_setup.sh
@@ -5,5 +5,4 @@ echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
-echo [$(date)]: "export conda environment"
-conda env export > conda.yaml
+
